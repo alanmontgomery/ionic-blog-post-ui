155233f83866c2d3a3495fe55d76fb3050e5e520
alanmontgomery <alan.montgomery@tascomi.com>
2021-08-04 22:06:53 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import BlogPost from './pages/BlogPost';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,7 +23,7 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
+const App = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
@@ -31,6 +32,10 @@ const App: React.FC = () => (
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
+        </Route>
+
+        <Route exact path="/post/:id">
+          <BlogPost />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
